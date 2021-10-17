0e0b2b381959c95a3e7996947a6cfb240c18d487
Aryan Karan <aryankaran28022004@gmail.com>
2021-10-17 16:15:48 +0530

    Create havoc_onclite.mk

---
+++ havoc_onclite.mk
@@ -0,0 +1,40 @@
+#
+# Copyright (C) 2019-2020 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
+
+# Inherit from onclite device
+$(call inherit-product, device/xiaomi/onclite/device.mk)
+
+# Inherit some common Havoc stuff.
+$(call inherit-product, vendor/havoc/config/common_full_phone.mk)
+
+TARGET_BOOT_ANIMATION_RES := 720
+#TARGET_INCLUDE_PIXEL_CHARGER := true
+
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_GAPPS_ARCH := arm64
+
+PRODUCT_NAME := havoc_onclite
+PRODUCT_DEVICE := onclite
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := Redmi 7
+PRODUCT_MANUFACTURER := Xiaomi
+
+BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ3A.210705.001/7380771:user/release-keys"
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="redfin-user 11 RQ3A.210705.001 7380771 release-keys"
+    
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.build.fingerprint=$(BUILD_FINGERPRINT)
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+HAVOC_BUILD_TYPE := Official
